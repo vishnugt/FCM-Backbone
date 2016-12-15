5af8a4fa48499f106e0c90e50c063fc677c35075
Meliodas <vishnugt@hotmail.com>
2016-12-16 03:19:46 +0530

    Sockets First push

--- app/src/main/java/com/npincomplete/fcm/basefcm/MainActivity.java
+++ app/src/main/java/com/npincomplete/fcm/basefcm/MainActivity.java
@@ -1,13 +1,64 @@
 package com.npincomplete.fcm.basefcm;
 
+import android.app.Activity;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.http.message.BasicNameValuePair;
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import de.tavendo.autobahn.WebSocketConnection;
+import de.tavendo.autobahn.WebSocketException;
+import de.tavendo.autobahn.WebSocketHandler;
+
+import static android.R.attr.host;
+
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start();
     }
+    private static final String TAG = "de.tavendo.test1";
+
+    private final WebSocketConnection mConnection = new WebSocketConnection();
+
+    private void start() {
+        final String wsuri = "ws://4e16c88d.ngrok.io/admin/notif";
+        try {
+            mConnection.connect(wsuri, new WebSocketHandler() {
+                @Override
+                public void onOpen() {
+                    Log.d(TAG, "Status: Connected to " + wsuri);
+                    mConnection.sendTextMessage("Hello, world!");
+                }
+
+                @Override
+                public void onTextMessage(String payload) {
+                    Log.d(TAG, "Got echo: " + payload);
+                }
+
+                @Override
+                public void onClose(int code, String reason) {
+                    Log.d(TAG, "Connection lost.");
+                }
+            });
+        } catch (WebSocketException e) {
+
+            Log.d(TAG, e.toString());
+        }
+    }
+
+
 }
